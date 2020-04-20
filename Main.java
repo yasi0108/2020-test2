158860b5162a982f50cc98eb602a1700feee929c
Yasin Guler <naber@hotmail.com>
2020-04-20 11:27:47 +0200

    indført spænderne for løkke

--- Main.java
+++ Main.java
@@ -3,5 +3,9 @@ public class main {
 
 public static void Main(String[] args) {
 System.out.println("Hello world of Git!");
+System.out.println("endnu flere!");
+int x=5;
+for (int i=0; i<3; i++)
+System.outprintln(x);
 	}
 }
